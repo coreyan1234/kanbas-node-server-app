fd12113bb99b4e573e03c5e4fcc4253ce64b3f7e
coreyan1234 <an.co@northeastern.edu>
2024-04-01 18:47:06 -0400

    First commit

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    _id: "RS101",
+    name: "React JS",
+    number: "CS1234",
+    startDate: "2024-01-10",
+    endDate: "2024-05-15",
+    image: "reactjs.webp",
+  },
+  {
+    _id: "RS102",
+    name: "Mobile App Dev",
+    number: "CS4520",
+    startDate: "2024-01-10",
+    endDate: "2024-05-15",
+    image: "mobile_app.jpg",
+  },
+  {
+    _id: "RS103",
+    name: "Web Development",
+    number: "CS4550",
+    startDate: "2024-01-10",
+    endDate: "2024-05-15",
+    image: "web_dev.png",
+  },
+];
