e4fb7d3c3ee5615138968ae27997776fd4844cfe
coreyan1234 <coreyan1234@gmail.com>
2024-04-13 16:56:27 -0400

    Implemented through 3.5.3

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import Lab5 from './Lab5.js';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import cors from "cors";
+import mongoose from 'mongoose';
+import UserRoutes from './Kanbas/users/routes.js';
 
+mongoose.connect("mongodb://localhost:27017"); // mongodb://127.0.0.1:27017/kanbas
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,4 +15,5 @@ CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen(4000);
