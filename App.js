6bbe89cfafd8c80593c5aaf0ef60f0e52be19858
coreyan1234 <coreyan1234@gmail.com>
2024-04-16 03:16:20 -0400

    Updated the mongoDB connection string to work with the new database

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ import "dotenv/config";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
-// mongoose.connect("mongodb://127.0.0.1:27017/kanbas"); // mongodb://127.0.0.1:27017/kanbas
 const app = express();
 app.use(cors({
     credentials: true,
